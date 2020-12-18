3efba89c499196a78422cb30688d5d312f0102cb
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2020-12-18 16:27:09 -0500

    update types and utils

--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,18 @@ export type TokenConfig = {
   contractAddress: string;
   decimals: number;
 };
+
+export type ApiData = {
+  bab_spot: number;
+  bab_supply: number;
+  bac_spot: number;
+  bac_total_supply: number;
+  bac_twap: number;
+  bas_spot: number;
+  bas_total_supply: number;
+  boardroom_bas: number;
+  daibac_stakingpool_bas: number;
+  daibas_stakingpool_bas: number;
+  daibas_uniswap_bas: number;
+  next_seignorage_epoch: number;
+};
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,28 @@
 import { commify } from 'ethers/lib/utils';
+import { SEIGNORAGE_EVENTS_PER_DAY } from './constants';
+
+export const formatValue = (value: number) => commify(value.toFixed(2));
+
+const getSupplyIncreaseRaw = (bacSupply: number, bacTwap: number): number =>
+  bacSupply * (bacTwap - 1);
 
 /*
  * Gets the number of projected minted BAC tokens based on the issuance
  * equation published in https://docs.basis.cash/
  */
 export const getSupplyIncrease = (bacSupply: number, bacTwap: number) =>
-  commify((bacSupply * (bacTwap - 1)).toFixed(2));
+  getSupplyIncreaseRaw(bacSupply, bacTwap);
+
+/* Gets the amount of BAC returned per issued BAS share staked in the boardroom */
+export const getReturnPerBas = (
+  bacSupply: number,
+  bacTwap: number,
+  stakedBasInBoardroom: number,
+) => getSupplyIncreaseRaw(bacSupply, bacTwap) / stakedBasInBoardroom;
+
+/* Gets the return in USD (DAI) terms per BAS based on the next expansion */
+export const getBasReturnDaily = (
+  bacPrice: number,
+  bacReturnPerBas: number,
+  basPrice: number,
+) => ((bacReturnPerBas * bacPrice) / basPrice) * SEIGNORAGE_EVENTS_PER_DAY;
