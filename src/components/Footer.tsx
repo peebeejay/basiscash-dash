19f7784a5bdb6df929e6c06578b8b741a9083355
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2020-12-18 20:17:28 -0500

    optimizations to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,6 +10,7 @@ const Container = styled.footer`
   justify-content: center;
   align-items: center;
   margin-bottom: ${rem(5)};
+  font-size: ${rem(14)};
 `;
 
 const MadeBy = styled.div`
@@ -20,12 +21,18 @@ const Links = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
+  letter-spacing: -0.5px;
 `;
 
 const Link = styled.a`
   color: black;
   margin-right: ${rem(10)};
   text-decoration: none;
+  transition: 250ms color ease;
+
+  &:hover {
+    color: purple;
+  }
 `;
 
 const Mode = styled.div`
@@ -41,17 +48,30 @@ const Divider = styled.div`
 export const Footer = (props: Props) => {
   return (
     <Container>
-      <MadeBy>{'made by saitama + junto'}</MadeBy>
+      <MadeBy>{'made by saitama & junto'}</MadeBy>
       <Divider>{'|'}</Divider>
       <Links>
-        <Link href="https://twitter.com">twitter</Link>
-        <Link href="https://twitter.com">discord</Link>
-        <Link href="https://twitter.com">telegram</Link>
-        <Link href="https://twitter.com">coingecko</Link>
-        <Link href="https://twitter.com">github</Link>
+        <Link href="https://twitter.com/BasisCash" target={'_blank'}>
+          twitter
+        </Link>
+        <Link href="https://discord.gg/UEZq3HF5Eu" target={'_blank'}>
+          discord
+        </Link>
+        <Link href="https://t.me/basiscash" target={'_blank'}>
+          telegram
+        </Link>
+        <Link href="https://www.coingecko.com/en/coins/basis-cash" target={'_blank'}>
+          coingecko
+        </Link>
+        <Link href="https://github.com/Basis-Cash" target={'_blank'}>
+          github
+        </Link>
+        <Link href="https://medium.com/basis-cash" target={'_blank'}>
+          medium
+        </Link>
       </Links>
       <Divider>{'|'}</Divider>
-      <Mode>theme options</Mode>
+      <Mode>theme (soon)</Mode>
     </Container>
   );
 };
