757bb720a17ba90e7511cd48feb5ff4f602fbb0f
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2020-12-21 16:57:56 -0500

    remove secrets from frontend

--- src/api.ts
+++ src/api.ts
@@ -1,47 +1,4 @@
-import { Fetcher, Route, Token, ChainId } from '@uniswap/sdk';
-import { ethers, Contract } from 'ethers';
-import { infuraConfig } from './secrets';
-import { config } from './config';
-import { tokens } from './constants';
-import { ApiData, TokenConfig } from './types';
-import ERC20_ABI from './constants/ERC20_abi.json';
-import { formatUnits } from 'ethers/lib/utils';
-
-export const apiGetTokenPriceFromUniswap = async (
-  tokenConfig: TokenConfig,
-): Promise<string | undefined> => {
-  const provider = new ethers.providers.InfuraProvider(ChainId.MAINNET, infuraConfig);
-
-  const daiConfig = tokens.dai;
-  const dai = new Token(config.chainId, daiConfig.contractAddress, daiConfig.decimals);
-  const token = new Token(
-    config.chainId,
-    tokenConfig.contractAddress,
-    tokenConfig.decimals,
-  );
-
-  try {
-    const daiToToken = await Fetcher.fetchPairData(dai, token, provider);
-    const priceInDAI = new Route([daiToToken], token);
-    return priceInDAI.midPrice.toSignificant(3);
-  } catch (err) {
-    console.error(`Failed to fetch token price of ${tokenConfig.symbol}: ${err}`);
-  }
-};
-
-export const apiGetTotalSupply = async (
-  token: TokenConfig,
-): Promise<string | undefined> => {
-  const provider = new ethers.providers.InfuraProvider(ChainId.MAINNET, infuraConfig);
-  const contract = new Contract(token.contractAddress, ERC20_ABI, provider);
-
-  try {
-    const supply = await contract.totalSupply();
-    return Number(formatUnits(supply, token.decimals)).toFixed(0);
-  } catch (err) {
-    console.error(`Failed to fetch supply of token ${token.symbol}`);
-  }
-};
+import { ApiData } from './types';
 
 export const apiGetData = async (): Promise<ApiData | undefined> => {
   const uri = 'https://basiscash-server-nextjs-nine.vercel.app/api/data';
--- src/secrets.ts
+++
@@ -1,4 +0,0 @@
-/* Will need to get a new projectID if code is made public */
-export const infuraConfig = {
-  projectId: '45c35f7e54d347c5b794bd0424b76d66',
-};
